464577740d4fe157bdbff714100241b446973f40
Owen <owenoxj@gmail.com>
2025-03-28 18:24:05 +0800

    Create main.c

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+
+
+
+int main(){
+
+    char day [20];
+    char timeSched [20];
+    char subject [20]; 
+    char section [20]; 
+
+
+    printf("Enter day schedule: ");
+    scanf("%c", &day);
+
+    printf("Enter day schedule: ");
+    scanf("%c", &timeSched);
+
+    printf("Enter Subject: ");
+    scanf("%c", &subject);
+
+    printf("Enter Section: ");
+    scanf("%c", &section);
+
+
+    return 0;
+}
