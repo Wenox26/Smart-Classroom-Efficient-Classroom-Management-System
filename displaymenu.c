ccdd99382b012a3512b01381a4bb95dd9a49f341
Owen <owenoxj@gmail.com>
2025-03-30 15:47:24 +0800

    Display Menu

--- displaymenu.c
+++ displaymenu.c
@@ -1,18 +1,46 @@
-#include <stdio.h>
-#include <string.h>
-#include <stdlib.h>
-
-
-
-
-
-
-int main(){
-
-
-    printf("WELOCME TO COMPUTER LABORATORY MANAGEMENT SYSTEM!");
-    printf
-
-
+int main() {
+    int choice;
+    
+    do {
+        printf("\n");
+        printf("      ____________________\n");
+        printf("     |                    |\n");
+        printf("     |   COMPUTER LAB     |\n");
+        printf("     |   SCHEDULE SYSTEM  |\n");
+        printf("     |____________________|\n");
+        printf("          ||      ||\n");
+        printf("          ||      ||\n");
+        printf("          ||______||\n");
+        printf("          |______|\n");
+        printf("\n");
+        printf("====================================\n");
+        printf("|     COMPUTER LAB SCHEDULE MENU   |\n");
+        printf("====================================\n");
+        printf("| 1. New Schedule                 |\n");
+        printf("| 2. Display All Schedules        |\n");
+        printf("| 3. Display Day Schedule         |\n");
+        printf("| 4. Exit                         |\n");
+        printf("====================================\n");
+        printf("Enter your choice: ");
+        scanf("%d", &choice);
+        
+        switch (choice) {
+            case 1:
+                printf("\n[New Schedule Functionality]\n");
+                break;
+            case 2:
+                printf("\n[Display All Schedules Functionality]\n");
+                break;
+            case 3:
+                printf("\n[Display Day Schedule Functionality]\n");
+                break;
+            case 4:
+                printf("\nExiting the program...\n");
+                break;
+            default:
+                printf("\nInvalid choice! Please try again.\n");
+        }
+    } while (choice != 4);
+    
     return 0;
 }
