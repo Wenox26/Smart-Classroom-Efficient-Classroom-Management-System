08719b80b457e7628a93a8a9bec63502cb13d38d
Owen <owenoxj@gmail.com>
2025-03-30 14:42:50 +0800

    Update displaymenu.c

---
+++ displaymenu.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+
+
+
+
+int main(){
+
+
+    printf("WELOCME TO COMPUTER LABORATORY MANAGEMENT SYSTEM!");
+    printf
+
+
+    return 0;
+}
