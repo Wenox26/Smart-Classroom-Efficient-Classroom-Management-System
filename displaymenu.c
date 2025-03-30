9e6be7d0f9741dce33cadf619924dab0cb63c977
Owen <owenoxj@gmail.com>
2025-03-30 18:41:20 +0800

    Update

--- displaymenu.c
+++ displaymenu.c
@@ -1,38 +1,31 @@
 #include <stdio.h>
 
+void asciiArt();
 
 int main() {
     int choice;
     
-    do {
-        printf("\n");
-        printf("      ____________________\n");
-        printf("     |                    |\n");
-        printf("     |   COMPUTER LAB     |\n");
-        printf("     |   SCHEDULE SYSTEM  |\n");
-        printf("     |____________________|\n");
-        printf("          ||      ||\n");
-        printf("          ||      ||\n");
-        printf("          ||______||\n");
-        printf("          |________|\n");
-        printf("\n");
-        printf("====================================\n");
-        printf("|     COMPUTER LAB SCHEDULE MENU   |\n");
-        printf("====================================\n");
-        printf("| 1. New Schedule                 |\n");
-        printf("| 2. Display All Schedules        |\n");
-        printf("| 3. Display Day Schedule         |\n");
-        printf("| 4. Exit                         |\n");
-        printf("====================================\n");
-        printf("Enter your choice: ");
-        scanf("%d", &choice);
+    asciiArt();
+    
+   //32 blank spaces
+
+    printf("                                ====================================\n");
+    printf("                                |     COMPUTER LAB SCHEDULE MENU   |\n");
+    printf("                                ====================================\n");
+    printf("                                | 1. New Schedule                 |\n");
+    printf("                                | 2. Display All Schedules        |\n");
+    printf("                                | 3. Display Day Schedule         |\n");
+    printf("                                | 4. Exit                         |\n");
+    printf("                                ====================================\n");
+    printf("                                Enter your choice: ");
+    scanf("%d", &choice);
         
         switch (choice) {
             case 1:
-                printf("\n[New Schedule Functionality]\n");
+                printf("\n                                [New Schedule Functionality]\n");
                 break;
             case 2:
-                printf("\n[Display All Schedules Functionality]\n");
+                printf("\n                                [Display All Schedules Functionality]\n");
                 break;
             case 3:
                 printf("\n[Display Day Schedule Functionality]\n");
@@ -43,7 +36,36 @@ int main() {
             default:
                 printf("\nInvalid choice! Please try again.\n");
         }
-    } while (choice != 4);
+    
     
     return 0;
+}
+
+
+void asciiArt(){
+    printf("                                +==============================================================================================+ \n"); 
+    printf("                                | ######   #######  ##     ## ##          ###    ########                                      |\n");
+    printf("                                |##    ## ##     ## ###   ### ##         ## ##   ##     ##                                     |\n");
+    printf("                               	|##       ##     ## #### #### ##        ##   ##  ##     ##                                     |\n");
+    printf("                                |##       ##     ## ## ### ## ##       ##     ## ########                                      |\n");
+    printf(" #@@@@@@@@ #@@@@@@@@ @@@= @@@#  |##       ##     ## ##     ## ##       ######### ##     ##                                     |\n");
+    printf(" #@@= @@@@ #@@@#@@@@ @@@= @@@#  |##    ## ##     ## ##     ## ##       ##     ## ##     ##                                     |\n");
+    printf(" #@@@#@@@# #@@* *@@@ @@@= @@@#  | ######   #######  ##     ## ######## ##     ## ########                                      |\n");
+    printf(" #@@@ @@@@ #@@* *@@@ @@@= @@@#  |                                                                                              |\n");
+    printf(" #@@@@@@@@ #@@* *@@@ @@@= @@@#  |##     ##    ###    ##    ##    ###     ######   ######## ##     ## ######## ##    ## ########|\n");
+    printf(" #@@=  @@@ #@@@# +@@ @@@= @@@#  |###   ###   ## ##   ###   ##   ## ##   ##    ##  ##       ###   ### ##       ###   ##    ##   |\n");
+    printf(" #@@=  @@@ *@@@@@. = @@@= @@@#  |#### ####  ##   ##  ####  ##  ##   ##  ##        ##       #### #### ##       ####  ##    ##   |\n");
+    printf(" #@@@  @@@ = -#@@@@= @@@= @@@#  |## ### ## ##     ## ## ## ## ##     ## ##   #### ######   ## ### ## ######   ## ## ##    ##   |\n");
+    printf(" #@@@@@@@@ #@#  @@@@ @@@= @@@#  |##     ## ######### ##  #### ######### ##    ##  ##       ##     ## ##       ##  ####    ##   |\n");
+    printf(" #@@=  @@@ #@@* *@@@ @@@= @@@#  |##     ## ##     ## ##   ### ##     ## ##    ##  ##       ##     ## ##       ##   ###    ##   |\n");
+    printf("  #@@@@@#  #@@* *@@@ @@@@@@@#	|##     ## ##     ## ##    ## ##     ##  ######   ######## ##     ## ######## ##    ##    ##   |\n");
+    printf("   *= #@@@ #@@* *@@@ @@@@@@*    |                                                                                              |\n");
+    printf("       @@@ #@@* *@@@ @@@@#      | ######  ##    ##  ######  ######## ######## ##     ##                                        |\n");
+    printf("       +#@ #@@@@@@@@ @#+        |##    ##  ##  ##  ##    ##    ##    ##       ###   ###                                        |\n");
+    printf("            :=**+=:             |##         ####   ##          ##    ##       #### ####                                        |\n");
+    printf("                                | ######     ##     ######     ##    ######   ## ### ##                                        |\n");
+    printf("                                |      ##    ##          ##    ##    ##       ##     ##                                        |\n");
+    printf("                                |##    ##    ##    ##    ##    ##    ##       ##     ##                                        |\n");
+    printf("                                | ######     ##     ######     ##    ######## ##     ##                                        |\n");
+    printf("                                +==============================================================================================+\n");
 }
